7b9570f8db0e21abb58d0ac3c9db1921433a3917
OkCripto <criptounity@gmail.com>
2026-01-06 21:06:01 +0530

    added Authentication and Authorization

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,4 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth";
+import _ from "lodash";
 
 export interface Project {
     id: number;
@@ -73,13 +75,39 @@ export interface Team {
   projectManagerUserId?: number;
 }
 
-
-
 export const api = createApi({
-    baseQuery: fetchBaseQuery({baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL}),
+    baseQuery: fetchBaseQuery({
+        baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+        prepareHeaders: async (headers) => {
+            const session = await fetchAuthSession();
+            const { accessToken } = session.tokens ?? {};
+            if (accessToken) {
+                headers.set("Authorization", `Bearer ${accessToken}`);
+            }
+            return headers;
+        }
+    }),
     reducerPath: "api",
     tagTypes: ["Projects", "Tasks", "Users", "Teams"],
     endpoints: (build) => ({
+        getAuthUser: build.query({
+            queryFn: async(_, _queryApi, _extraoptions, fetchWithBQ) => {
+                try{
+                    const user = await getCurrentUser();
+                    const session = await fetchAuthSession();
+                    if(!session) throw new Error("No Session Found");
+                    const { userSub } = session;
+                    const { accessToken } = session.tokens ?? {};
+
+                    const userDetailsResponse = await fetchWithBQ('users/${userSub}');
+                    const userDetails = userDetailsResponse.data as User;
+
+                    return { data: {user, userSub, userDetails} };
+                } catch(error:any) {
+                    return {error: error.message || "Could Not Fetch User Data"};
+                }
+            } 
+        }),
         getProjects: build.query<Project[], void>({
             query: () => "projects",
             providesTags: ["Projects"]
@@ -140,4 +168,15 @@ export const api = createApi({
     }),
 });
 
-export const {useGetProjectsQuery, useCreateProjectMutation, useGetTasksQuery, useCreateTaskMutation, useUpdateTaskStatusMutation, useSearchQuery, useGetUsersQuery, useGetTeamsQuery, useGetTasksByUserQuery} = api;             
+export const {
+    useGetProjectsQuery, 
+    useCreateProjectMutation, 
+    useGetTasksQuery,
+    useCreateTaskMutation,
+    useUpdateTaskStatusMutation, 
+    useSearchQuery, 
+    useGetUsersQuery, 
+    useGetTeamsQuery, 
+    useGetTasksByUserQuery,
+    useGetAuthUserQuery
+    } = api;             
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
-import {  getUsers, postUser } from "../controllers/userController.js";
+import {  getUser, getUsers, postUser } from "../controllers/userController.js";
 
 const router = Router();
 
 router.get("/", getUsers);
 router.post("/", postUser)
+router.get('/:cognitoId', getUser);
 
 
 export default router;
